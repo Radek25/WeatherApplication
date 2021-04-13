a057e02db2c1010c5510d3b2b1c7abd9bfe84428
My Name <66422260+Radek25@users.noreply.github.com>
2021-04-13 13:27:23 +0200

    Create SaveDataInLocalStorage functionalilty

--- save-in-local-storage.js
+++ save-in-local-storage.js
@@ -1,4 +1,8 @@
+const LSKey = 'Weather';
+let DataToLocalStorage = [];
+
 let Data = {
+    Id : null,
     Location : null,
     Country : null,
     Icon : null,
@@ -9,5 +13,28 @@ let Data = {
     Humi : null, //Humidity
 }
 function SaveDataFromAPI(DataFromApi) {
-    console.log(DataFromApi);
+    if(WeatherCounter < 4){
+    Data.Id = new Date().getTime();
+    Data.Location = DataFromApi.name;
+    Data.Country = DataFromApi.sys.country;
+    Data.Icon = DataFromApi.weather[0].icon;
+    Data.Desc = DataFromApi.weather[0].description;
+    Data.Temp = Math.floor((DataFromApi.main.temp - 273.15), 2);
+    Data.Pres = DataFromApi.main.pressure;
+    Data.Wind = Math.floor(DataFromApi.wind.speed);
+    Data.Humi = DataFromApi.main.humidity;
+
+    PushDataToArray();
+    }
+}
+
+function PushDataToArray() {
+    console.log(Data);
+    DataToLocalStorage.push(JSON.parse(JSON.stringify(Data)));
+    console.log(DataToLocalStorage);
+    SetToLocalStorage();
+}
+
+function SetToLocalStorage() {
+    localStorage.setItem(LSKey, JSON.stringify(DataToLocalStorage));
 }
